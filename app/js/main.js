f6604f6f32803fceb06e5fd2c872def2298b987b
Kirill Shlenkin <Shlyonkin.K@yandex.ru>
2022-02-18 00:33:51 +0300

    rightside-menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
+    $('.header__button').on('click', function() {
+        $('.rightside-menu').removeClass('rightside-menu--close')
+    });
+    
+    $('.rightside-menu__close').on('click', function() {
+        $('.rightside-menu').addClass('rightside-menu--close')
+    })
 
 })
